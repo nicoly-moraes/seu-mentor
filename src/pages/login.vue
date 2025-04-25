d0a22df3dc95cf7017ac2c5029bf4242406db077
Nicoly Alcantara Moraes <nicoly.alcantara23@gmail.com>
2025-04-25 16:33:32 -0300

    pagina de login e validacao de email

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,3 +1,94 @@
 <template>
-  <p>Pagina de login</p>
+  <section>
+    <h1 class="titulo">
+      Faça o seu login
+    </h1>
+  </section>
+ <v-form class="form">
+    <v-row class="box">
+
+      <v-col
+        cols="12"
+      >
+        <v-text-field
+          v-model="email"
+          :rules="[validation.required, validation.email]"
+          label="E-mail"
+          data-vv-name="email"
+          variant="solo-filled"
+          required
+
+        ></v-text-field>
+      </v-col>
+
+      <v-col
+        cols="12"
+      >
+        <v-text-field
+        v-model="senha"
+        :rules="[validation.required]"
+        type="password"
+        label="Senha"
+        data-vv-name="Senha"
+        variant="solo-filled"
+        required
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="12" class="text-center">
+        <v-btn class="btn-entrar" color="primary" size="large" @click="logar">Entrar</v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
 </template>
+
+<script lang="ts">
+import { validarEmail } from '@/utils/validations.util';
+
+export default {
+  name: "login",
+  data() {
+    return {
+      email: "",
+      senha: "",
+      validation: {
+        required: (value: string) => !!value || 'Campo é obrigatorio',
+        email: (value: string) => validarEmail(value) || 'Email inválido'
+      }
+    }
+  },
+  methods: {
+    logar() {
+      if(!this.email || !this.senha || !validarEmail(this.email)) {
+        return;
+      }
+      console.log("🚀 ~ login realizado com sucesso:")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.titulo{
+  margin-top: 50px;
+  padding: 30px;
+  text-align: center;
+}
+
+.form {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 67px;
+}
+
+.box {
+  max-width: 450px;
+}
+
+.btn-entrar {
+  margin-bottom: 10px;
+  border-radius: 12px;
+  border: 20px;
+}
+
+</style>
